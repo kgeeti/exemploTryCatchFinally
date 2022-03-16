ee93f6e3ebf65603ab0a05758fa096c89cfff01d
kgeeti <kge@terra.com.br>
2022-03-16 15:00:57 -0300

    Primeiro Commit Local v1.0

---
+++ src/main/java/br/eti/kge/exemplotrycatchfinally/PrimeiraClasse.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.eti.kge.exemplotrycatchfinally;
+
+/**
+ *
+ * @author KGe
+ */
+public class PrimeiraClasse {
+    public static void main(String[] args) {
+        System.out.println("Primeira Classe");
+    }
+}
